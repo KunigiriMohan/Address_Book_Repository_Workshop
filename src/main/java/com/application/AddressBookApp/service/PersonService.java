6774c3200e524be8426bbb781f8609be54eb50d3
KunigiMohan <kmohanr5@gmail.com>
2021-12-30 09:15:02 +0530

    Added : Creating Service Class of Application.

---
+++ src/main/java/com/application/AddressBookApp/service/PersonService.java
@@ -0,0 +1,10 @@
+package com.application.AddressBookApp.service;
+
+import org.springframework.stereotype.Service;
+/**
+ * @Service : Service Layer of Application
+ */
+@Service
+public class PersonService {
+    
+}
