43d54d0f8ed1c677630ad25b29ad24027abe74a4
KunigiMohan <kmohanr5@gmail.com>
2021-12-30 09:19:55 +0530

    Added : Person DTO class.

---
+++ src/main/java/com/application/AddressBookApp/dto/PersonDTO.java
@@ -0,0 +1,16 @@
+package com.application.AddressBookApp.dto;
+
+public class PersonDTO {
+    
+    public String firstName;
+    public String lastName;
+    public String gender;
+    public Long mobileNo;
+    public String emailAddress;
+    public String city;
+    public String state;
+    public String country;
+    public String address;
+    public String profilepic;
+
+}
