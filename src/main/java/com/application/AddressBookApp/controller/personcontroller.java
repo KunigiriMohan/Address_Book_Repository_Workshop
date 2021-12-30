4ee38e7dab99c53f70dc77808b1e98ad91abe4ee
KunigiMohan <kmohanr5@gmail.com>
2021-12-30 09:11:50 +0530

    Added : Controller Class of Application.

---
+++ src/main/java/com/application/AddressBookApp/controller/personcontroller.java
@@ -0,0 +1,12 @@
+package com.application.AddressBookApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @RestController : Creating Controller Class
+ * @RequestMapping : Mapping Controller Class to URL
+ */
+@RestController
+@RequestMapping("/addressbook")
+public class personcontroller {
+}
