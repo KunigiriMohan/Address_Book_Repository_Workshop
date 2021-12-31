63dd9ff2cb99f880e7c7171868f9dd8e5b0947bd
KunigiMohan <kmohanr5@gmail.com>
2021-12-31 09:21:11 +0530

    Added : Refactoring Code to save in another Repository.

--- src/main/java/com/application/AddressBookApp/repository/PersonRepository.java
+++ src/main/java/com/application/AddressBookApp/repository/PersonRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
  * @Repository : Creating Reposory to store data in DataBase
  */
 @Repository
-public interface PersonRepository extends JpaRepository<Person,Long>{
-    
+public interface PersonRepository extends JpaRepository<Person,Long>{  
 }
