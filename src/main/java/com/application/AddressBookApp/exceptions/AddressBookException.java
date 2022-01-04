9ccbd16f2c1a6184a02276d0e6aa31e158e67484
KunigiMohan <kmohanr5@gmail.com>
2022-01-04 22:29:37 +0530

    Added : Exception Handler of Applicaation.

--- src/main/java/com/application/AddressBookApp/exceptions/AddressBookException.java
+++ src/main/java/com/application/AddressBookApp/exceptions/AddressBookException.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 /**
  * @ControllerAdvice : Creating Controller Advice to Handle Exceptions
  * @ExceptionHandler : Creating Exception Handler method to handle Exceptions Thrown
