03aec2fa383893aa1753e66d29a3a8b3e7fae3a5
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2022-01-05 12:25:08 +0530

    Added : Renaming Service Layer Interface.

---
+++ src/main/java/com/application/AddressBookApp/service/IPersonService.java
@@ -0,0 +1,16 @@
+package com.application.AddressBookApp.service;
+
+import java.util.List;
+import com.application.AddressBookApp.dto.PersonDTO;
+import com.application.AddressBookApp.model.*;
+
+public interface IPersonService {
+    /**
+     * Methods of Service Layer
+     */
+    List<Person> getPersonData();
+    Person getPersonDataById(Long personID);
+    Person createPersonData(Long addressBookID,PersonDTO personDTO);
+    Person updatePersonData(Long addressBookID,Long personID, PersonDTO personDTO);
+    void deletePersonData(Long personID);
+}
